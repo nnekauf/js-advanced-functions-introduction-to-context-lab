96183a3e6e2984959c698399e05adb6fef9b789c
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-05-12 22:22:30 -0400

    commit tree b6a25a533fdedf14eb313adc22a100910a97db8a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function createEmployeeRecord(array) {
+    let employee = {};
+    employee.firstName = array[0];
+    employee.familyName = array[1];
+    employee.title = array[2];
+    employee.payPerHour = array[3];
+    employee.timeInEvents = [];
+    employee.timeOutEvents = [];
+    return employee
+}
+
+function createEmployeeRecords(array) {
+   
+}
